f1f66ef74e6ff1f28d49d900692b357d9d9d23bf
Narco Shilukukwa <137599138+Narcob47@users.noreply.github.com>
2025-03-04 11:34:59 +0200

    static files

--- studios/studios/settings.py
+++ studios/studios/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -108,10 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Path to your static files directory
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this is a full filesystem path
 
 # Enable WhiteNoise compression and caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
