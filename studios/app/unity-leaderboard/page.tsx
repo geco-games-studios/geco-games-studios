64635f982855a29c1727cbf3aa7df045ca501183
Luyando Shilukukwa <Luyando.shilukukwa@gmail.com>
2026-06-20 02:09:33 +0200

    Add Unity-fed online leaderboard prototype

---
+++ studios/app/unity-leaderboard/page.tsx
@@ -0,0 +1,7 @@
+import UnityLeaderboardClient from "./leaderboard-client"
+
+export const dynamic = "force-dynamic"
+
+export default function UnityLeaderboardPage() {
+  return <UnityLeaderboardClient />
+}
